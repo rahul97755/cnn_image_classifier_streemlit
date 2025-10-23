ff2124e045361753562d58064561cf657606384f
rahuld <rahul.dgp97755@gmail.com>
2025-10-23 16:37:15 +0530

    model loader model path corrected

--- src/model_loader.py
+++ src/model_loader.py
@@ -4,4 +4,4 @@ from tensorflow.keras.models import load_model as keras_load_model # type: ignor
 @st.cache_resource
 def load_trained_model():
     """Load and cache CNN model"""
-    return keras_load_model("models/cifar10_cnn_augmented.h5") # type: ignore
+    return keras_load_model("model/cifar10_cnn_augmented.h5") # type: ignore
